d633b7a2834d0ab5804e392c8adb9b470b6656cf
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-15 11:17:27 -0300

    Fix NPE on isMandatoryCodec
    05-14 00:51:30.825 E/AndroidRuntime(13043): FATAL EXCEPTION: main
    05-14 00:51:30.825 E/AndroidRuntime(13043): Process: com.android.bluetooth, PID: 13043
    05-14 00:51:30.825 E/AndroidRuntime(13043): java.lang.RuntimeException: Error receiving broadcast Intent { act=android.bluetooth.a2dp.profile.action.CONNECTION_STATE_CHANGED flg=0x5000010 (has extras) } in com.android.bluetooth.a2dp.A2dpService$ConnectionStateChangedReceiver@74406fd
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0(LoadedApk.java:1401)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.app.-$$Lambda$LoadedApk$ReceiverDispatcher$Args$_BumDX2UKsnxLVrE6UJsJZkotuA.run(Unknown Source:2)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.os.Handler.handleCallback(Handler.java:873)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.os.Handler.dispatchMessage(Handler.java:99)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.os.Looper.loop(Looper.java:193)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.app.ActivityThread.main(ActivityThread.java:6746)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at java.lang.reflect.Method.invoke(Native Method)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:495)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:858)
    05-14 00:51:30.825 E/AndroidRuntime(13043): Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean android.bluetooth.BluetoothCodecConfig.isMandatoryCodec()' on a null object reference
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpCodecConfig.enableOptionalCodecs(A2dpCodecConfig.java:73)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpService.enableOptionalCodecs(A2dpService.java:1013)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpService.updateOptionalCodecsSupport(A2dpService.java:1335)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpService.connectionStateChanged(A2dpService.java:1351)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpService.access$400(A2dpService.java:63)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at com.android.bluetooth.a2dp.A2dpService$ConnectionStateChangedReceiver.onReceive(A2dpService.java:1384)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0(LoadedApk.java:1391)
    05-14 00:51:30.825 E/AndroidRuntime(13043): 	... 8 more
    
    Change-Id: If9fd3642ae3e8f83073bf4c26576687e04a34711

--- src/com/android/bluetooth/a2dp/A2dpCodecConfig.java
+++ src/com/android/bluetooth/a2dp/A2dpCodecConfig.java
@@ -70,7 +70,7 @@ class A2dpCodecConfig {
         // Set the mandatory codec's priority to default, and remove the rest
         for (int i = 0; i < codecConfigArray.length; i++) {
             BluetoothCodecConfig codecConfig = codecConfigArray[i];
-            if (!codecConfig.isMandatoryCodec()) {
+            if (codecConfig == null || !codecConfig.isMandatoryCodec()) {
                 codecConfigArray[i] = null;
             }
         }
@@ -86,7 +86,7 @@ class A2dpCodecConfig {
         // Set the mandatory codec's priority to highest, and remove the rest
         for (int i = 0; i < codecConfigArray.length; i++) {
             BluetoothCodecConfig codecConfig = codecConfigArray[i];
-            if (codecConfig.isMandatoryCodec()) {
+            if (codecConfig != null && codecConfig.isMandatoryCodec()) {
                 codecConfig.setCodecPriority(BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST);
             } else {
                 codecConfigArray[i] = null;
